318563932869b0f1bf75e2ddc34a7834409a1578
Wojtyla <144253449+RaplesWojtyla@users.noreply.github.com>
2025-01-18 03:44:52 +0700

    add: dotenv configuration

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
 import { generateRecipeWithHuggingFace, generateRecipeWithGemini } from "./api/aiService.js"
+
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT
